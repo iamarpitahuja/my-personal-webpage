cbf11f661e9f32e56648e533fa6975af9831f799
iamarpitahuja <iamarpitahuja@gmail.com>
2025-06-22 22:30:13 -0400

    Update script.js
    navbar animation added

--- ui-handler/script.js
+++ ui-handler/script.js
@@ -12,3 +12,39 @@ window.addEventListener('load', function() {
         }, 2000); // match fade duration
     }, 200); // delay after load
 });
+
+//content animations and functions
+document.addEventListener('DOMContentLoaded', function() {
+    //navbar functions
+    const navbar = document.querySelector('.navbar');
+    const menuToggle = document.getElementById('mobile-menu');
+    const navLinks = document.getElementById('nav-links');
+
+
+    //navbar scroll and hide funcs
+    let lastScrollY = window.scrollY;
+    window.addEventListener('scroll', () => {
+        if (window.scrollY <= 50) {
+            navbar.classList.remove('hidden');
+        } else if (window.scrollY > lastScrollY) {
+            navbar.classList.add('hidden');
+        }
+        lastScrollY = window.scrollY;
+    });
+
+    //small (mobile) screen nav
+    if(menuToggle && navLinks) {
+        menuToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    }
+
+    //make mobile menu close
+    const navLinkItems = document.querySelectorAll('.nav-links a');
+    navLinkItems.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+        });
+    });
+    
+});
